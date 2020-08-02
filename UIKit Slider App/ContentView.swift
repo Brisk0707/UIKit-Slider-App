fcf3d82ce805addc8a7dbac423c9ab6ccd64c23b
Borys Krisko <bvkr@me.com>
2020-08-02 21:12:01 +0300

    finish

--- UIKit Slider App/ContentView.swift
+++ UIKit Slider App/ContentView.swift
@@ -10,35 +10,40 @@ import SwiftUI
 
 struct ContentView: View {
     @State var value = 0.0
-    @State var alphaValue = CGFloat(0)
-    var randomValue = Int.random(in: 0...100)
+    @State var randomValue = Int.random(in: 0...100)
+    @State var alertIsShowing = false
     var body: some View {
-        // Text("Hello, World!")
         
         VStack {
             Text("Подвиньте слайдер как можно ближе к: \(randomValue)")
             HStack {
                 Text("0")
-                SliderUiKit(alphaValue: $alphaValue, value: $value)
+                SliderUiKit(value: $value,
+                            randomValue: $randomValue)
                     
                     .frame(width: 300)
                 Text("100")
             }
-            Button(action: {}) {
+            Button(action: {self.alertIsShowing = true}) {
                 Text("Проверь меня!")
             }.padding()
-           
-            Button(action: {}) {
+                .alert(isPresented: $alertIsShowing, content: {
+                    Alert(title: Text("Your score is"),
+                          message: Text("\(computeScore())"),
+                        dismissButton: .default(Text("Ok")))
+                })
+            
+            Button(action: {self.randomValue = Int.random(in: 0...100)}) {
                 Text("Начать заново!")
             }
         }
         
     }
-    
-    private func computeScore() -> Int {
+    public func computeScore() -> Int {
         let difference = abs(randomValue - lround(value))
         return 100 - difference
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
